aee8f3feef81ddf8eccabcb3a664b02655634103
Jason <jason.palomino@sjsu.edu>
2025-03-22 01:27:10 -0700

    defined exceptions for invalid format and obj count

--- FastFoodSystem/src/App.java
+++ FastFoodSystem/src/App.java
@@ -56,123 +56,130 @@ public class App {
 
         while(true) {
             String input = getInput("");
-            switch(input) {
-                //Have static methods in each class for having user input create objects
-                case "1":
-                    int registerId = Integer.valueOf(getInput("Give this register an id"));
-                    if(registerList.containsKey(registerId)) {
-                        System.out.println("Error: Duplicate ID");
-                        break;
-                    }
-                    registerList.put(registerId, new CashRegister(registerId));
-                    break;
-                case "2":
-                    int customerID = Integer.valueOf(getInput("Enter customer ID: "));
-                    if(customerList.containsKey(customerID)) {
-                        System.out.println("Error: Duplicate ID");
-                        break;
-                    }
-                    String customerName = getInput("Enter customer name: ");
-                    double customerBalance = Double.parseDouble(getInput("What is this customer's balance?: "));
-                    Customer newCustomer = new Customer(customerID, customerName, false, customerBalance);
-                    customerList.put(customerID, newCustomer);
-                    break;
-                case "3":
-                    int selectedCustomerID = Integer.valueOf(getInput("Which customer will use a register? (Enter customer ID)"));
-                    if(customerList.containsKey(selectedCustomerID)) {
-                        Customer customerToUseRegister = customerList.get(selectedCustomerID);
-                        int registerToBeUsedID = Integer.valueOf(getInput("Which register will " + customerToUseRegister.getName() + " use?:"));
-                        CashRegister usedRegister = registerList.get(registerToBeUsedID);
-                        customerToUseRegister.useRegister(usedRegister);
-                    } else {
-                        System.out.println("Error: Invalid ID");
-                    }
-                    break;
-                case "4":
-                    int orderingCustomerID = Integer.valueOf(getInput("Which customer will order? (Enter customer ID)"));
-                    if(customerList.containsKey(orderingCustomerID) ) {
-                        Customer customerOrdering = customerList.get(orderingCustomerID);
-                        if(customerOrdering.getRegister() == null) {
-                            System.out.println("Error: Customer is not at a register");
+            try {
+                switch(input) {
+                    //Have static methods in each class for having user input create objects
+                    case "1":
+                        int registerId = Integer.valueOf(getInput("Give this register an id"));
+                        if(registerList.containsKey(registerId)) {
+                            System.out.println("Error: Duplicate ID");
                             break;
                         }
-                        if(customerOrdering.getCurrentOrder() != null) {
-                            System.out.println("A customer can only have one order at a time. Refund to make new order");
+                        registerList.put(registerId, new CashRegister(registerId));
+                        break;
+                    case "2":
+                        int customerID = Integer.valueOf(getInput("Enter customer ID: "));
+                        if(customerList.containsKey(customerID)) {
+                            System.out.println("Error: Duplicate ID");
                             break;
                         }
-                        printMenu();
-                        System.out.println("What will " + customerOrdering.getName() + " order? (Enter 1-3, type anything else to exit)");
-                        Order order = new Order(orderingCustomerID, customerList.get(orderingCustomerID)); //For now, orderingID will be the customerID
-                        while(true) {
-                            String option = getInput("Add item: ");
-                            if(option.equals("1")) {
-                                order.addItem(new Burger());
-                            } else if(option.equals("2")) {
-                                order.addItem(new Fry());
-                            } else if(option.equals("3")) {
-                                order.addItem(new Milkshake());
-                            } else {
-                                customerOrdering.placeOrder(order);
+                        String customerName = getInput("Enter customer name: ");
+                        double customerBalance = Double.parseDouble(getInput("What is this customer's balance?: "));
+                        Customer newCustomer = new Customer(customerID, customerName, false, customerBalance);
+                        customerList.put(customerID, newCustomer);
+                        break;
+                    case "3":
+                        int selectedCustomerID = Integer.valueOf(getInput("Which customer will use a register? (Enter customer ID)"));
+                        if(customerList.containsKey(selectedCustomerID)) {
+                            Customer customerToUseRegister = customerList.get(selectedCustomerID);
+                            int registerToBeUsedID = Integer.valueOf(getInput("Which register will " + customerToUseRegister.getName() + " use?:"));
+                            CashRegister usedRegister = registerList.get(registerToBeUsedID);
+                            customerToUseRegister.useRegister(usedRegister);
+                        } else {
+                            System.out.println("Error: Invalid ID");
+                        }
+                        break;
+                    case "4":
+                        int orderingCustomerID = Integer.valueOf(getInput("Which customer will order? (Enter customer ID)"));
+                        if(customerList.containsKey(orderingCustomerID) ) {
+                            Customer customerOrdering = customerList.get(orderingCustomerID);
+                            if(customerOrdering.getRegister() == null) {
+                                System.out.println("Error: Customer is not at a register");
                                 break;
                             }
-                        }     
-                        
-                    } else {
-                        System.out.println("Error: Invalid ID");
-                    }
-                    break;
-                case "5":
-                    int employeeID = Integer.valueOf(getInput("Enter employee ID: "));
-                    if(employeeList.containsKey(employeeID)) {
-                        System.out.println("Error: Duplicate ID");
+                            if(customerOrdering.getCurrentOrder() != null) {
+                                System.out.println("A customer can only have one order at a time. Refund to make new order");
+                                break;
+                            }
+                            printMenu();
+                            System.out.println("What will " + customerOrdering.getName() + " order? (Enter 1-3, type anything else to exit)");
+                            Order order = new Order(orderingCustomerID, customerList.get(orderingCustomerID)); //For now, orderingID will be the customerID
+                            while(true) {
+                                String option = getInput("Add item: ");
+                                if(option.equals("1")) {
+                                    order.addItem(new Burger());
+                                } else if(option.equals("2")) {
+                                    order.addItem(new Fry());
+                                } else if(option.equals("3")) {
+                                    order.addItem(new Milkshake());
+                                } else {
+                                    customerOrdering.placeOrder(order);
+                                    break;
+                                }
+                            }     
+                            
+                        } else {
+                            System.out.println("Error: Invalid ID");
+                        }
                         break;
-                    }
-                    String employeeName = getInput("Enter employee name: ");
-                    employeeList.put(employeeID, new Employee(employeeID, employeeName, true, "", 0, 0));
-                    break;
-                case "6":
-                    break;
-                case "7":
-                    int refundCustomerID = Integer.valueOf(getInput("Which customer will be refunding? (Enter customer ID)"));
-                    Customer refundingCustomer = customerList.get(refundCustomerID);
-                    if(customerList.containsKey(refundCustomerID) ) {
-                        if(refundingCustomer.getRegister() == null) {
-                            System.out.println("Error: Customer is not at a register");
+                    case "5":
+                        int employeeID = Integer.valueOf(getInput("Enter employee ID: "));
+                        if(employeeList.containsKey(employeeID)) {
+                            System.out.println("Error: Duplicate ID");
                             break;
                         }
-                        if(refundingCustomer.getCurrentOrder() != null) {
-                            refundingCustomer.issueRefund(refundingCustomer.getCurrentOrder().calculateTotal());
+                        String employeeName = getInput("Enter employee name: ");
+                        employeeList.put(employeeID, new Employee(employeeID, employeeName, true, "", 0, 0));
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        int refundCustomerID = Integer.valueOf(getInput("Which customer will be refunding? (Enter customer ID)"));
+                        Customer refundingCustomer = customerList.get(refundCustomerID);
+                        if(customerList.containsKey(refundCustomerID) ) {
+                            if(refundingCustomer.getRegister() == null) {
+                                System.out.println("Error: Customer is not at a register");
+                                break;
+                            }
+                            if(refundingCustomer.getCurrentOrder() != null) {
+                                refundingCustomer.issueRefund(refundingCustomer.getCurrentOrder().calculateTotal());
+                            } else {
+                                System.out.println("Error: Customer has no order");
+                            } 
                         } else {
-                            System.out.println("Error: Customer has no order");
-                        } 
-                    } else {
-                        System.out.println("Error: Invalid ID");
-                    }
-                    break;
-                case "8":
-                    System.out.println("------------Customers------------");
-                    for (Map.Entry<Integer, Customer> entry : customerList.entrySet()) {
-                        Customer customer = entry.getValue();
-                        customer.displayInfo();
-                    }
-                    System.out.println("------------Registers------------");
-                    for (Map.Entry<Integer, CashRegister> entry : registerList.entrySet()) {
-                        CashRegister register = entry.getValue();
-                        register.displayRegisterStats();
-                    }
-                    break;
-                case "9":
-                    break;
-                case "10":
-                    break;
-                case "11":
-                    break;
-                default:
-                    System.out.println("Invalid input");
-                    break;
+                            System.out.println("Error: Invalid ID");
+                        }
+                        break;
+                    case "8":
+                        System.out.println("------------Customers------------");
+                        for (Map.Entry<Integer, Customer> entry : customerList.entrySet()) {
+                            Customer customer = entry.getValue();
+                            customer.displayInfo();
+                        }
+                        System.out.println("------------Registers------------");
+                        for (Map.Entry<Integer, CashRegister> entry : registerList.entrySet()) {
+                            CashRegister register = entry.getValue();
+                            register.displayRegisterStats();
+                        }
+                        break;
+                    case "9":
+                        break;
+                    case "10":
+                        break;
+                    case "11":
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                        break;
 
+                }
+                printUI();
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please try again.");
+                printUI();
+            } catch(IllegalStateException e) {
+                System.out.println("Object count has exceeded 100");
             }
-            printUI();
         }
     }
 }
